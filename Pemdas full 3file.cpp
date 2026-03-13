b4a8899d66edce1c57121da075b7ff5a17ab8aee
aryaom74-max <aryaom74@gmail.com>
2026-03-14 01:13:36 +0700

    Refactor and clean up code structure

--- Pemdas full 3file.cpp
+++ Pemdas full 3file.cpp
@@ -99,7 +99,7 @@ int main(){
         cout << "nilai x sudah dikoreksi menjadi : " << x << endl;
     }
 
-    cout << "Bilangan yang diberikan = " << x;
+    cout << " yang diberikan = " << x;
 
     return 0;
 }
